d57b9baef770fc3d62c195ae6e180a70dce3f70f
prabuddhaxdev <prabuddha1749@gmail.com>
2026-08-04 23:41:33 +0530

    chore(websocket): update package.json and pnpm-lock.yaml for JWT and server-common integration

--- apps/websocket/src/index.ts
+++ apps/websocket/src/index.ts
@@ -1,13 +1,27 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { JWT_SECRET } from '@repo/server-common/config';
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", function connection(ws) {
-  ws.on("error", console.error);
+wss.on("connection", function connection(ws, request) {
+  const url = request.url;
+  if (!url) {
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") || "";
+
+  const secret = JWT_SECRET || "";
+  const decoded = jwt.verify(token, secret);
+
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
 
   ws.on("message", function message(data) {
-    console.log("received: %s", data);
+    ws.send("something");
   });
-
-  ws.send("pong");
 });
